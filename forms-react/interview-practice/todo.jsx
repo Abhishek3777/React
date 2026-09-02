a3a80775d68d0d096c42e926ef4e2a6ab5bbc72d
Abhishek3777 <kr7abhishek04@gmail.com>
2026-09-02 09:34:31 +0530

    add todo component with cleanup reminder

--- forms-react/interview-practice/todo.jsx
+++ forms-react/interview-practice/todo.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const todo = () => {
   return (
     <div>
-      
+      <h1>
+        Further cleanup and structuring required in repo on the weekend
+      </h1>
     </div>
   )
 }
